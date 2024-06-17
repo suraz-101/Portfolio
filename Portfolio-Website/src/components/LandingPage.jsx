32d830577c5796144d15a38695e39cee1eff3ebf
Suraj Pandey <surazpandey101@gmail.com>
2024-06-17 23:04:20 +0545

    landing page styling

--- Portfolio-Website/src/components/LandingPage.jsx
+++ Portfolio-Website/src/components/LandingPage.jsx
@@ -3,18 +3,24 @@ import handWaving from "../assets/waving-hand-svgrepo-com.svg";
 export const LandingPage = () => {
   return (
     <div>
-      <h1 className="text-center mt-2 d-flex">
+      <h1 className="d-flex justify-content-center mt-2 d-flex">
         <span>
-          <img src={handWaving} alt="" height="50px" width="50px" />
+          <img
+            src={handWaving}
+            alt=""
+            height="60px"
+            width="60px"
+            className="mx-3"
+          />
         </span>
         Hellow, I am{" "}
         <span className="fw-bold name">
-          <ul className="list-group text-decoration-none unlisted lists">
-            <li className="list-group-item text-start list">
-              <span>Suraj Pandey</span>
+          <ul className=" text-decoration-none unlisted lists">
+            <li className="text-start list">
+              <span>Suraj Pandey .</span>
             </li>
-            <li className="list-group-item list">
-              <span>Full Stack Develper</span>
+            <li className=" list">
+              <span>Full Stack Develper .</span>
             </li>
           </ul>
         </span>

b05084b46ba5baeccf64278846e175f69d187dc6
Suraj Pandey <surazpandey101@gmail.com>
2024-06-17 22:12:31 +0545

    landing page design

--- Portfolio-Website/src/components/LandingPage.jsx
+++ Portfolio-Website/src/components/LandingPage.jsx
@@ -1,5 +1,24 @@
 import React from "react";
-
+import handWaving from "../assets/waving-hand-svgrepo-com.svg";
 export const LandingPage = () => {
-  return <div>Welcome to my portfolio website</div>;
+  return (
+    <div>
+      <h1 className="text-center mt-2 d-flex">
+        <span>
+          <img src={handWaving} alt="" height="50px" width="50px" />
+        </span>
+        Hellow, I am{" "}
+        <span className="fw-bold name">
+          <ul className="list-group text-decoration-none unlisted lists">
+            <li className="list-group-item text-start list">
+              <span>Suraj Pandey</span>
+            </li>
+            <li className="list-group-item list">
+              <span>Full Stack Develper</span>
+            </li>
+          </ul>
+        </span>
+      </h1>
+    </div>
+  );
 };

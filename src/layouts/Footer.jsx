49711502152f71b0b875d1d70fc5b5edab5185d5
Suraj Pandey <surazpandey101@gmail.com>
2024-08-18 23:25:50 +0545

    Animation

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="z-30 bg-yellow-600 p-6 bg-secondaryBg text-textColor">
-      Footer of my porfolio website
+    <div className="z-30 bg-yellow-600 p-6 bg-secondaryBg text-textColor text-center">
+      Copyright @2024 All right Reserved |This template is made by{" "}
+      <i className="fa fa-user"></i> Suraj Pandey
     </div>
   );
 };

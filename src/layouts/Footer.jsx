9788dadb78bf8640615b2f3f6634a08472a5894a
Suraj Pandey <surazpandey101@gmail.com>
2025-02-24 18:16:18 +0000

    footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 export const Footer = () => {
+  let date = new Date();
+  let year = date.getFullYear();
   return (
     <div className="z-30  p-6 bg-secondaryBg text-textColor text-center">
-      Copyright @2024 All right Reserved |This template is made by{" "}
+      Copyright @{year} All right Reserved |This template is made by{" "}
       <i className="fa fa-user"></i> Suraj Pandey
     </div>
   );
